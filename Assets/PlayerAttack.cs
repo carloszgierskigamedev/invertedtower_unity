882dc8e1627a21b7e891eb38c8e59a2ff204a392
Carlos Alexandre Zgierski <carloszgierski@gmail.com>
2021-03-16 02:48:34 -0300

    Added a very simple enemy hit test. Gotta improve.

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [Header("Attack variables")]
+    [SerializeField] private float _attackRange = 1f;
+    [SerializeField] private float _attackRadius = 1f;
+    [Header("Layer")]
+    [SerializeField] private LayerMask _enemyLayer = default;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleAttack();
+    }
+
+    private void HandleAttack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Attack!");
+            Vector3 mousePosition = GetMouseWorldPosition();
+            Vector3 attackDirection = (mousePosition - transform.position).normalized;
+            Vector3 attackPosition = transform.position + attackDirection * _attackRange; 
+            Debug.Log(attackPosition);
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPosition, _attackRadius, _enemyLayer);
+
+            foreach(Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("Hit");
+            }
+        }
+    }
+
+
+    // Get mouse position in world with Z = 0f
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
